6849bf658ff4bc2c832382b9ff54a51f51241ee0
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2025-11-09 19:09:38 +0530

    Frontend Setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-linear-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden min-h-screen">
+      <h1 className="text-red-500 text-5xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
