5e43d5acf77f54e7059f68c3ee421278d1259f60
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2025-11-09 11:33:00 +0530

    Created User Model

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/connectDB.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Hello world 123');
 });
+
+app.use("/api/auth",authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
