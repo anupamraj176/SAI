d13c76116f9709c1847fdaffc95ad8ab7f974095
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2026-08-31 13:48:25 +0530

    feat: initialize chatbot implementation with LangGraph and Groq API configuration

---
+++ chatbot/chatbot.py
@@ -0,0 +1,36 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_groq import ChatGroq
+from langgraph.graph.message import add_messages
+
+class ChatState(TypedDict) :
+    messages : Annotated[list[BaseMessage],add_messages]
+
+graph = StateGraph(ChatState)
+
+llm = ChatGroq(model="openai/gpt-oss-20b")
+def chat_node(state:ChatState):
+    #take user query from message
+    messages = state['messages']
+    #call llm
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+
+#add node
+graph.add_node('chat_node',chat_node)
+
+#ADD Edge
+graph.add_edge(START,'chat_node')
+graph.add_edge('chat_node',END)
+chatbot = graph.compile()
+
+
+intialState = {"messages": [HumanMessage(content='How are you')]}
+result= chatbot.invoke(intialState)
+print(result['messages'][-1].content)
