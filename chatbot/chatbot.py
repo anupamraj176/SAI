fc317dcce020db04e5e032894c771cf26c3c1b45
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2026-08-31 15:24:42 +0530

    feat: implemented basic persistent memory

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,15 +1,23 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 import os
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 from dotenv import load_dotenv
 
 load_dotenv()
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_groq import ChatGroq
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import InMemorySaver
+
 
 class ChatState(TypedDict) :
     messages : Annotated[list[BaseMessage],add_messages]
+
+
+checkpointer = InMemorySaver()
 
 graph = StateGraph(ChatState)
 
@@ -28,13 +36,16 @@ graph.add_node('chat_node',chat_node)
 #ADD Edge
 graph.add_edge(START,'chat_node')
 graph.add_edge('chat_node',END)
-chatbot = graph.compile()
+chatbot = graph.compile(checkpointer=checkpointer)
 
 
+config = {'configurable' : {'thread_id' : '1'}}
 intialState = {"messages": [HumanMessage(content='What is the capital of india')]}
-result= chatbot.invoke(intialState)
+result= chatbot.invoke(intialState, config=config)
 print(result['messages'][-1].content)
 
+
+thread_id = '1'
 
 while True :
     user_message = input('Type Here : ')
@@ -43,8 +54,9 @@ while True :
     if user_message.strip().lower() in ['exit','quit']:
         print("Chatbot Terminated")
         break
-
+    
+    config = {'configurable' : {'thread_id' : thread_id}}
     intialState = {"messages": [HumanMessage(content=user_message)]}
-    result= chatbot.invoke(intialState)
+    result= chatbot.invoke(intialState,config=config)
     print(result['messages'][-1].content)
     
