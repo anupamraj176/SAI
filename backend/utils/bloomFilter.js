27f9f9e88db4b8b7781896a681734ceb29cbdc2e
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2026-09-16 10:57:40 +0530

    feat: initialize email bloom filter utility

---
+++ backend/utils/bloomFilter.js
@@ -0,0 +1,25 @@
+import { BloomFilter } from "bloom-filters";
+import {Account} from "../models/account.model.js";
+import { addTransitionType } from "react";
+
+//create a bloom filter that can hold 100,000 emails with a 1% false positive rate
+export const emailBloomFilter = new BloomFilter(100000,0.01);
+
+export const initializeBloomFilter = async() => {
+    try{
+        console.log("Intializing Bloom filter ....");
+        const accounts = await Account.find({},{
+            email:1,
+            _id:0
+        });
+        accounts.forEach(acc => {
+            if(acc.email){
+                emailBloomFilter.add(acc.email);
+            }
+        });
+        console.log(`Bloom Filter Fully Loaded with ${accounts.length} emails`);
+    }
+    catch(error){
+        console.error("Failed to intialize  Bloom Filter : " , error);
+    }
+}
